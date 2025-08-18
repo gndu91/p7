c6623f3df2a948b6d23a7293f8b68f2be39a3a96
Nadir GHOUL <nadir@ghoul.fr>
2025-08-18 12:31:56 +0200

    Added a script to launch mlflow correctly, and adding extra requirements needed to run with prometheus.

---
+++ scripts/mfu.sh
@@ -0,0 +1,23 @@
+# Starts the ml flow ui using the correct environment, at the correct location
+echo "Navigating to project's root..."
+cd "$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")"
+echo "Navigating to project's root...done"
+
+echo "Activating environment..."
+source "$(pwd).13.env/bin/activate"
+echo "Activating environment...done"
+
+echo "Launching mlflow ui..."
+
+# --expose-prometheus: This probably won't be used, but it's enabled in case I decide to use it
+# --dev TODO: Remove this later
+mkdir -p "$(pwd)".{prometheus,mlflow}
+mlflow ui \
+	--host 127.65.12.247 \
+	--port 50222 \
+	--backend-store-uri "$(pwd).mlflow" \
+	--backend-store-uri "$(pwd).mlflow" \
+	--workers 32 \
+	--expose-prometheus "$(pwd).prometheus" \
+	--dev 
+echo "Launching mlflow ui...done"

e3af3ee5308401870e4bddf6690e2b47c4601c23
Nadir GHOUL <nadir@ghoul.fr>
2025-08-18 08:57:48 +0200

    Added scripts to simplify the development

---
+++ scripts/prefix.sh
@@ -0,0 +1,16 @@
+# All the scripts will start the same way, by relocating and activating the environment
+
+test "${BASH_SOURCE[0]}" == "$0" && {
+  echo WARNING: This script needs to be sourced, not run > /dev/stderr
+  exit 1
+}
+
+# TODO: Auto create environment
+
+echo "Navigating to project's root..."
+cd "$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")"
+echo "Navigating to project's root...done"
+
+echo "Activating environment..."
+source "$(pwd).13.env/bin/activate"
+echo "Activating environment...done"
---
+++ scripts/update.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+echo "Navigating to project's root..."
+cd "$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")"
+echo "Navigating to project's root...done"
+
+echo "Activating environment..."
+source "$(pwd).13.env/bin/activate"
+echo "Activating environment...done"
+
+echo "Updating..."
+pip install -Ur requirements.txt
+echo "Updating...done"
+
+echo "Freezing..."
+pip freeze > frozen-requirements.txt
+echo "Freezing...done"

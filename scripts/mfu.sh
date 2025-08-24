a7e552305945e8dad3fda581c148cfd928c2f738
Nadir GHOUL <nadir@ghoul.fr>
2025-08-24 17:05:31 +0200

    Mise à jour des dossiers utilisés

--- scripts/mfu.sh
+++ scripts/mfu.sh
@@ -11,13 +11,16 @@ echo "Launching mlflow ui..."
 
 # --expose-prometheus: This probably won't be used, but it's enabled in case I decide to use it
 # --dev TODO: Remove this later
-mkdir -p "$(pwd)".mlflow/{prometheus,mlflow,artifacts}
+mkdir -p "$(pwd)".mlflow
+pushd "$(pwd)".mlflow
+# mkdir -p {prometheus,mlflow,artifacts}
 mlflow ui \
 	--host 127.65.12.247 \
 	--port 50222 \
-	--backend-store-uri "$(pwd).mlflow/mlflow" \
-	--artifacts-destination "$(pwd).mlflow/artifacts" \
+	--backend-store-uri "mlflow" \
+	--artifacts-destination "artifacts" \
 	--workers 32 \
-	--expose-prometheus "$(pwd).mlflow/prometheus" \
-	--dev 
+	--expose-prometheus "prometheus" \
+	--dev
+popd
 echo "Launching mlflow ui...done"
